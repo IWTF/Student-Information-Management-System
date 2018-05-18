947b5b5c6e55a1ef2563cbf4147a53a45fb824a6
IWTF <33515162+IWTF@users.noreply.github.com>
2018-05-19 07:33:24 +0800

    Create Modify.c

---
+++ Modify.c
@@ -0,0 +1,22 @@
+#include<fstream>
+using namespace std;
+#include"Master.h"
+#include"Node.h"
+extern Node n;
+extern void cao(Master &m);
+void Modify(Master &m1){
+	int z=0;
+	cout<<"          修改学生信息模块"<<endl
+		<<"-------------------------------------"<<endl;
+	n.Modify(m1);
+	cout<<"__1.返回首页"<<endl
+		<<"__2.继续修改"<<endl;
+	cin>>z;
+	if(z==1){
+		system("cls");
+		cao(m1);
+	}else{
+		system("cls");
+		Modify(m1);
+	}
+}
